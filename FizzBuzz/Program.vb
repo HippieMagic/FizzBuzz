4a2db37555bbf1d39b94a272cab15c41d0caa991
Nick Moore <nick_r_moore@hotmail.com>
2018-04-29 14:32:57 -0400

    Written for clarity.

--- FizzBuzz/Program.vb
+++ FizzBuzz/Program.vb
@@ -2,6 +2,20 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        For index = 1 To 100
+            Dim output As String = String.Empty
+
+            If index Mod 15 = 0 Then
+                output = "FizzBuzz"
+            ElseIf index Mod 3 = 0 Then
+                output = "Fizz"
+            ElseIf index Mod 5 = 0 Then
+                output = "Buzz"
+            Else
+                output = CType(index, String)
+            End If
+
+            Console.WriteLine(output)
+        Next
     End Sub
 End Module
